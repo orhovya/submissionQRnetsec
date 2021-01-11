37f47abec2022b63d47b6e7db0d5e09c6c0bc31a
amitmao <amitmao@post.bgu.ac.il>
2021-01-12 00:38:10 +0200

    add video

--- qrCode/main.py
+++ qrCode/main.py
@@ -3,17 +3,13 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-import qrcode
 import pyqrcode
 
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    myfile = open("qrLink","w+")
-    url = pyqrcode.create('~/Desktop/submissionQRnetsec/submission_html/subsys.html')
-    url.svg('uca-url.svg', scale=8)
-    url.eps('uca-url.eps', scale=2)
+    url = pyqrcode.create('file:///home/amitmao/Desktop/submissionQRnetsec/submission_html/subsys.html')
+    url.svg('uca-url.svg',background='white', scale=8)
 
 
 
